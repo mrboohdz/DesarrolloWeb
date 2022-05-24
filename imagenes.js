e870835ec97147ce5e63114c8494ce69776c79fa
mrboohdz <315234208@pcpuma.acatlan.unam.mx>
2022-05-24 18:11:11 -0500

    Act 5

---
+++ imagenes.js
@@ -0,0 +1,14 @@
+function perritoFeli(){
+  var uno = document.getElementById("feli");
+  var dos = document.getElementById("nojao");
+    dos.style.display="none";
+    uno.style.display="block";
+}
+
+function perritoNojao(){
+  var uno = document.getElementById("feli");
+  var dos = document.getElementById("nojao");
+    uno.style.display="none";
+    dos.style.display="block";
+
+}
